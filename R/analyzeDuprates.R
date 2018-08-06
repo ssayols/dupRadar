0d0b3464a227eaa2fb6b049823b6de037367a8e3
zxBIB Sayols_Puig,Sergi (RES) EXTERNAL <sergi.sayols_puig.ext@boehringer-ingelheim.com>
2018-08-06 11:32:38 +0200

    support feature types other than exons

--- R/analyzeDuprates.R
+++ R/analyzeDuprates.R
@@ -36,44 +36,49 @@ analyzeDuprates <- function(bam,gtf,stranded=0,paired=FALSE,threads=1,
     if(!is.logical(paired))   stop("paired has to be either TRUE/FALSE")
     if(!is.numeric(stranded)) stop("stranded has to be a number [0-2]")
     if(stranded < 0 || stranded > 2) stop("stranded has to be a number [0-2]")
-    if(!is.numeric(threads)) stop("threads has to be a number")
+    if(!is.numeric(threads))  stop("threads has to be a number")
 
     # featureCounts simplified call
     count <- function(mh,dup) {
-        Rsubread::featureCounts(files=bam,annot.ext=gtf,
-			isGTFAnnotationFile=TRUE,GTF.featureType="exon",
-			GTF.attrType="gene_id",nthreads=threads, isPairedEnd=paired,
-			strandSpecific=stranded,ignoreDup=dup,countMultiMappingReads=mh,...)
+        Rsubread::featureCounts(files=bam,
+                                annot.ext=gtf,
+                                isGTFAnnotationFile=TRUE,
+                                nthreads=threads,
+                                isPairedEnd=paired,
+                                strandSpecific=stranded,
+                                ignoreDup=dup,
+                                countMultiMappingReads=mh,
+                                ...)
     }
 
     ## HK: status info for each call of featureCounts()? In silent mode it's
     ## not obvious any more what is happening. This would require assigning the
     ##results to individual variables first and generating the list afterwards.
     if (verbose) {
-        counts <- list(mhdup    =count(mh=TRUE ,dup=FALSE), 
-                       mhnodup  =count(mh=TRUE ,dup=TRUE ), 
-                       nomhdup  =count(mh=FALSE,dup=FALSE), 
+        counts <- list(mhdup    =count(mh=TRUE ,dup=FALSE),
+                       mhnodup  =count(mh=TRUE ,dup=TRUE ),
+                       nomhdup  =count(mh=FALSE,dup=FALSE),
                        nomhnodup=count(mh=FALSE,dup=TRUE ))
     }
     else { 
         ## assign output on STDOUT from featureCounts to variable silencer,
         ## which we ignore afterwards. 
         silencer <- capture.output(
-            counts <- list(mhdup    =count(mh=TRUE ,dup=FALSE), 
-                           mhnodup  =count(mh=TRUE ,dup=TRUE ), 
-                           nomhdup  =count(mh=FALSE,dup=FALSE), 
+            counts <- list(mhdup    =count(mh=TRUE ,dup=FALSE),
+                           mhnodup  =count(mh=TRUE ,dup=TRUE ),
+                           nomhdup  =count(mh=FALSE,dup=FALSE),
                            nomhnodup=count(mh=FALSE,dup=TRUE ))
             )
     }
         
     # calculate RPK & RPKM per gene
-    x <- lapply(counts,function(x) { 
+    x <- lapply(counts,function(x) {
         # total number of mapped reads
         N <- sum(x$stat[,2]) - x$stat[x$stat$Status == "Unassigned_Unmapped",2]
 
         x <- data.frame(gene=rownames(x$counts),
                         width=x$annotation$Length[match(rownames(x$counts),
-                                                  x$annotation$GeneID)],
+                                                        x$annotation$GeneID)],
                         counts=x$counts[,1],
                         RPK=0,
                         RPKM=0)
